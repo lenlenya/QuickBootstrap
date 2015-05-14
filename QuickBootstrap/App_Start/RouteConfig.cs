2461ef50e20bb5e1dec400974e131558463e405c
lenya <lenya@msn.cn>
2015-05-14 11:05:20 +0800

    增加动态二级域名的实现

--- QuickBootstrap/App_Start/RouteConfig.cs
+++ QuickBootstrap/App_Start/RouteConfig.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using QuickBootstrap.Helpers;
 
 namespace QuickBootstrap
 {
     public class RouteConfig
     {
+        static readonly string _domainRegular = null;
+
+        static RouteConfig()
+        {
+            _domainRegular = ConfigurationManager.AppSettings["DomainRegular"];
+            if (string.IsNullOrWhiteSpace(_domainRegular)) throw new NotImplementedException();
+        }
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add("DomainRoute", new DomainRoute(
+                 _domainRegular,       //Domain with parameters
+                 "{controller}/{action}/{id}",      //URL with parameters
+                 new { company = string.Empty, controller = "Home", action = "Index", id = UrlParameter.Optional }  //Parameter defaults
+                ));
+            routes.Add("DomainRoute_Page", new DomainRoute(
+                 _domainRegular,       //Domain with parameters
+                 "{controller}/{action}/{id}_{pageIndex}_{pageSize}",      //URL with parameters
+                 new { company = string.Empty, controller = "Home", action = "Index", id = UrlParameter.Optional }  //Parameter defaults
+                ));
 
             routes.MapRoute(
                 name: "Default",
---
+++ QuickBootstrap/Controllers/SecondDomainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickBootstrap.Controllers
+{
+    public class SecondDomainController : Controller
+    {
+        // GET: SecondDomain
+        public ActionResult Index(string company)
+        {
+            ViewBag.Company = company;
+            return View();
+        }
+    }
+}
